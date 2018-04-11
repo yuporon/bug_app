9a78563d9b9af0876944c6c50304afbb8492bfa2
Yuji Miyashita <ikkiiimonkey0229@gmail.com>
2018-04-11 19:07:37 +0900

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,62 @@
+class BlogsController < ApplicationController
+
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @blog = Blog.all
+  end
+
+  # GET /blogs/1
+  # GET /blogs/1.json
+  def show
+  end
+
+  # GET /blogs/new
+  def new
+    @blog = Blog.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to @blog, notice: 'Blog was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    if @blog.update(blog_params)
+      redirect_to @blog, notice: 'Blog was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @blog.destroy
+    redirect_to blogs_url, notice: 'Blog was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Blog.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def blog_params
+      params.require(:blog).permit(:title)
+    end
+end
