474798c4dd5cdaa4ebb7b0a1a52532e1db720be9
tomoki.itou <tomoki.itou@super-studio.jp>
2018-11-12 19:55:45 +0900

    Submission

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,10 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blog = Blog.all
+    @blogs = Blog.all
   end
 
   # GET /blogs/1
@@ -46,17 +47,18 @@ class BlogsController < ApplicationController
   # DELETE /blogs/1.json
   def destroy
     @blog.destroy
-    render :index
+    redirect_to blogs_path
   end
 
-  private
+private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:action])
+      @blog = Blog.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title)
+      params.require(:blog).permit(:title, :content)
     end
+
 end
